94f498b147aa83968d3ca5f7c2e62def139edc68
Adam Haubenstock <aehaubenstock.dev@gmail.com>
2020-02-07 22:30:37 -0500

    Update templates to new architecture

---
+++ Rx/Component.xctemplate/___FILEBASENAME___Component.swift
@@ -0,0 +1,12 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___VARIABLE_productName___Component: Component {
+}
---
+++ Rx/Screen.xctemplate/___FILEBASENAME___Screen.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class ___VARIABLE_productName___Screen: Screen {
+    static func addLogic(to component: <#Name#>Component, input: <#InputType#>, observer: AnyObserver<<#OutputType#>>) -> [Disposable] {
+        return [
+        ]
+    }
+}
